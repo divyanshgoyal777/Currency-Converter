6662e22bc50c27f10ff83f8b5797b6011c1ad0c7
divyanshgoyal777 <147051621+divyanshgoyal777@users.noreply.github.com>
2024-03-24 18:10:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies"
-
 const dropdowns = document.querySelectorAll(".dropdown select");
-const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg")
-let i = 0;
-
+const amount = document.querySelector(".amount input")
+const btn = document.querySelector(".Exchange")
 
 for (let select of dropdowns) {
     for (currencycode in countryList) {
@@ -27,21 +24,17 @@ for (let select of dropdowns) {
 }
 
 const updateExchangeRate = async () => {
-    let amount = document.querySelector(".amount input")
     let amtVal = amount.value;
     if (amtVal === "" || amtVal < 1) {
         amtVal = 1;
         amount.value = "1"
     }
-    // console.log(fromCurr.value, toCurr.value)
-    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-    let response = await fetch(URL);
-    let data = await response.json();
-    let rate = data[toCurr.value.toLowerCase()];
-    // console.log(rate);
-
-    let finalAmount = amtVal * rate;
-    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
+    const api_url = `https://v6.exchangerate-api.com/v6/bb08d43a5e91fac6e4985172/latest/${fromCurr.value}`
+    fetch(api_url).then(response => response.json()).then(result => {
+        let exchange_rate = result.conversion_rates[toCurr.value];
+        let totalExchangeRate = (amount.value * exchange_rate).toFixed(6);
+        msg.innerText = `${amount.value} ${fromCurr.value} = ${totalExchangeRate} ${toCurr.value}`
+    })
 }
 
 const updateFlag = (element) => {
@@ -52,11 +45,23 @@ const updateFlag = (element) => {
     img.src = newSrc;
 }
 
-
-btn.addEventListener("click", (evt) => {
-    evt.preventDefault();
+window.addEventListener("load", () => {
     updateExchangeRate();
 })
-window.addEventListener("load", () => {
+
+toCurr.addEventListener("click", () => {
+    updateExchangeRate();
+})
+
+fromCurr.addEventListener("click", () => {
+    updateExchangeRate();
+})
+
+amount.addEventListener("input", () => {
+    updateExchangeRate();
+})
+
+btn.addEventListener("click", (event) => {
+    event.preventDefault();
     updateExchangeRate();
 })
